75d50cd1ddef2fa152759388d05c738ed2c6e0d2
thereaper554 <135044402+thereaper554@users.noreply.github.com>
2025-12-10 20:01:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1TfEoZe0f_AmHMEU-JZkdzhBXGTH11l3D
 """
 
-!pip install streamlit
+
 import streamlit as st
 import pandas as pd
 import joblib
