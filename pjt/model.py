4c9c1f4301063ebf0a89434c2da94d0b09d0c273
thereaper554 <135044402+thereaper554@users.noreply.github.com>
2026-02-24 12:35:42 +0100

    Add files via upload

---
+++ pjt/model.py
@@ -0,0 +1,12 @@
+import joblib
+import pandas as pd
+
+def load_models(model_dir: str = "models"):
+    risk_model = joblib.load(f"{model_dir}/risk_model.joblib")
+    reg_model = joblib.load(f"{model_dir}/reg_model.joblib")
+    return risk_model, reg_model
+
+def infer(risk_model, reg_model, features_df: pd.DataFrame):
+    prisk = risk_model.predict(features_df)
+    prem = reg_model.predict(features_df)
+    return prisk, prem
